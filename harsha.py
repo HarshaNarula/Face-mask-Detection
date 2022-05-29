c29274003035eec7a448ec19a0d362861c254452
HarshaNarula <84308707+HarshaNarula@users.noreply.github.com>
2022-05-29 23:16:11 +0530

    Add files via upload

---
+++ harsha.py
@@ -0,0 +1,132 @@
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+from imutils.video import VideoStream
+import numpy as np
+import imutils
+import time
+import cv2
+import os
+from flask import Response
+from flask import Flask
+from flask import render_template
+import threading
+import datetime
+import imutils
+
+outputFrame = None
+lock = threading.Lock()
+app = Flask(__name__, template_folder="web")
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+
+@app.route("/")
+def index():
+    # return the rendered template
+    return render_template("webapp.html")
+
+print("Loading the face detector model.....")
+
+prototxtPath = r"deploy.protext"
+weightsPath = r"res10_300x300_ssd_iter_140000.caffemodel"
+faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
+
+print("Loading the face mask detector model.....")
+maskNet = load_model("mask_detector.model")
+
+def detect_and_predict_mask(frame):
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300), (104.0, 177.0, 123.0))
+
+    faceNet.setInput(blob)
+    detections = faceNet.forward()
+
+    faces = []
+    locs = []
+    preds = []
+
+    for i in range(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+
+        if confidence > 0.5:
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            (startX, startY) = (max(0, startX), max(0, startY))
+            (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+
+            face = frame[startY:endY, startX:endX]
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+            face = cv2.resize(face, (224, 224))
+            face = img_to_array(face)
+            face = preprocess_input(face)
+
+            faces.append(face)
+            locs.append((startX, startY, endX, endY))
+
+    if len(faces) > 0:
+        faces = np.array(faces, dtype="float32")
+        preds = maskNet.predict(faces, batch_size=32)
+
+    return (locs, preds)
+
+def detect():
+    global vs, outputFrame, lock
+    while True:
+        frame = vs.read()
+        frame = imutils.resize(frame, width=400)
+
+        (locs, preds) = detect_and_predict_mask(frame)
+
+        for (box, pred) in zip(locs, preds):
+            (startX, startY, endX, endY) = box
+            (mask, withoutMask) = pred
+
+            label = "Wearing Mask" if mask > withoutMask else "Not Wearing Mask"
+            color = (0, 255, 0) if label == "Wearing Mask" else (0, 0, 255)
+
+            label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+
+            cv2.putText(
+                frame,
+                label,
+                (startX, startY - 10),
+                cv2.FONT_HERSHEY_COMPLEX,
+                0.5,
+                color,
+                2,
+            )
+            cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+
+        with lock:
+            outputFrame = frame.copy()
+
+
+def generate():
+    global outputFrame, lock
+
+    while True:
+        with lock:
+            if outputFrame is None:
+                continue
+           
+            (flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
+            
+            if not flag:
+                continue
+        
+        yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
+              bytearray(encodedImage) + b'\r\n')
+
+@app.route("/video_feed")
+def video_feed():
+    return Response(generate(),
+                    mimetype="multipart/x-mixed-replace; boundary=frame")
+
+if __name__ == '__main__':
+    t = threading.Thread(target=detect)
+    t.daemon = True
+    t.start()
+
+    app.run(host="0.0.0.0", port=8000, debug=True,
+            threaded=True, use_reloader=False)
